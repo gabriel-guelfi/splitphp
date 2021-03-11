a68589e9a49b106bf1eb1267f79dadceab0893dd
Valentoni <gabriel.valguelfi@gmail.com>
2021-03-11 11:46:03 -0300

    Create home.php

---
+++ application/templates/site/home.php
@@ -0,0 +1,9 @@
+<img src="http://docs.dynamophp.org/wp-content/uploads/2021/03/dynamo-logo-medium.png">
+
+<h4><?php echo $message; ?></h4>
+
+<p>This is an example page, just to show how this works. Welcome message above comes from a service and is generated in run time</p>
+
+<p>For further information, please refer to the <a href="http://docs.dynamophp.org/" target="_blank">Dynamo Docs</a>.</p>
+
+<h6>Have fun! ;)</h6>
