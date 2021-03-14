51713f597d05e2521254401473cbd7810ce829d5
Valentoni <gabriel.valguelfi@gmail.com>
2021-03-14 05:01:23 -0300

    Update home.php

--- application/templates/site/home.php
+++ application/templates/site/home.php
@@ -1,9 +1,25 @@
-<img src="http://docs.dynamophp.org/wp-content/uploads/2021/03/dynamo-logo-medium.png">
 
-<h4><?php echo $message; ?></h4>
+<html>
 
-<p>This is an example page, just to show how this works. Welcome message above comes from a service and is generated in run time</p>
+<head>
+<title><?php echo APPLICATION_NAME; ?></title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
 
-<p>For further information, please refer to the <a href="http://docs.dynamophp.org/" target="_blank">Dynamo Docs</a>.</p>
+<body>
+    <img src="http://docs.dynamophp.org/wp-content/uploads/2021/03/dynamo-logo-medium.png">
 
-<p><b>Have fun! ;)</b></p>
+    <h4>Welcome to Dynamo, the lean, low curve modern PHP micro-framework</h4>
+
+    <p>This is an example page, just to show how this works. Welcome message above comes from a service and is generated in run time</p>
+
+    <p>For further information, please refer to the <a href="http://docs.dynamophp.org/" target="_blank">Dynamo Docs</a>.</p>
+
+    <p><b>Have fun! ;)</b></p>
+</body>
+
+</html>
