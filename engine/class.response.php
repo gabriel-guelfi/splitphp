73394752366d38f6977d895da302b598206ed212
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2022-03-25 20:40:17 -0300

    added doc comments up to Response class

--- engine/class.response.php
+++ engine/class.response.php
@@ -26,13 +26,44 @@
 //                                                                                                                                                                //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * Class Response
+ * 
+ * This class manages the response's information.
+ *
+ * @package engine
+ */
 class Response
 {
+  /**
+   * @var integer $status
+   * Stores the response's status code.
+   */
   private $status;
+  
+  /**
+   * @var string $contentType
+   * A string containing the response's content type header.
+   */
   private $contentType;
+
+  /**
+   * @var mixed $data
+   * Stores the payload which will be sent on the response.
+   */
   private $data;
+
+  /**
+   * @var array $headers
+   * An array of string representations of the response's custom headers.
+   */
   private $headers;
 
+  /** 
+   * Set the default response's status, content type, payload data and custom headers. Returns an instance of Response (constructor)
+   * 
+   * @return Response 
+   */
   public function __construct()
   {
     $this->status = 200;
@@ -41,31 +72,67 @@ class Response
     $this->headers = [];
   }
 
+  /** 
+   * Add a string representation of a header to the Response's headers collection. Each header stored on this collection, will be added to 
+   * the response, later.
+   * 
+   * @param string $header
+   * @return Response 
+   */
   public function setHeader(string $header)
   {
     $this->headers[] = $header;
     return $this;
   }
 
+  /** 
+   * Set the status code of the response.
+   * 
+   * @param integer $code
+   * @return Response 
+   */
   public function withStatus(int $code)
   {
     $this->status = $code;
     return $this;
   }
 
-  public function withText($text, $escape = true)
+  /** 
+   * Set the response's content type to "text/plain" and the response's payload data, with the data passed on $text.
+   * If $escape flag is set to false, the payload data will be set unescaped(insecure).
+   * 
+   * @param string $text
+   * @param boolean $escape = true
+   * @return Response 
+   */
+  public function withText(string $text, bool $escape = true)
   {
-    $this->data = $escape ? $this->escapeOutput($text) : $text;
+    $this->contentType = 'text/plain';
+    $this->data = $escape ? $this->sanitizeOutput($text) : $text;
     return $this;
   }
 
-  public function withData($data, $escape = true)
+  /** 
+   * Set the response's content type to "application/json" and the response's payload data, with the data passed on $data.
+   * If $escape flag is set to false, the payload data will be set unescaped(insecure).
+   * 
+   * @param mixed $data
+   * @param boolean $escape = true
+   * @return Response 
+   */
+  public function withData(mixed $data, bool $escape = true)
   {
     $this->contentType = 'application/json';
-    $this->data = $escape ? json_encode($this->escapeOutput($data)) : json_encode($data);
+    $this->data = $escape ? json_encode($this->sanitizeOutput($data)) : json_encode($data);
     return $this;
   }
 
+  /** 
+   * Set the response's content type to "text/html", encoded with utf-8 charset, and the response's payload data, with the data passed on $content.
+   * 
+   * @param string $content
+   * @return Response 
+   */
   public function withHTML(string $content)
   {
     $this->contentType = 'text/html; charset=utf-8';
@@ -73,6 +140,12 @@ class Response
     return $this;
   }
 
+  /** 
+   * Set the response's content type to "application/xml" and the response's payload data, with the data passed on $data.
+   * 
+   * @param mixed $data
+   * @return Response 
+   */
   public function withXMLData($data)
   {
     $this->contentType = 'application/xml';
@@ -80,32 +153,58 @@ class Response
     return $this;
   }
 
+  /** 
+   * Returns the collection of response's custom headers.
+   * 
+   * @return array 
+   */
   public function getHeaders()
   {
     return $this->headers;
   }
 
+  /** 
+   * Returns the response's status code.
+   * 
+   * @return integer 
+   */
   public function getStatus()
   {
     return $this->status;
   }
 
+  /** 
+   * Returns the response's content type string representation.
+   * 
+   * @return string 
+   */
   public function getContentType()
   {
     return $this->contentType;
   }
 
+  /** 
+   * Returns the response's payload data.
+   * 
+   * @return mixed 
+   */
   public function getData()
   {
     return $this->data;
   }
 
-  private function escapeOutput($payload)
+  /** 
+   * Sanitizes the passed data, encoding it with htmlspecialchars(), in order to avoid XSS attacks. Returns the sanitized data.
+   * 
+   * @param mixed $payload
+   * @return mixed 
+   */
+  private function sanitizeOutput(mixed $payload)
   {
     if (is_array($payload) || (gettype($payload) == 'object'))
       foreach ($payload as &$value) {
         if (gettype($value) == 'array' || (gettype($value) == 'object')) {
-          $value = $this->escapeOutput($value);
+          $value = $this->sanitizeOutput($value);
           continue;
         }
 
