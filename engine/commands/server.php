6557aefd8144246997254d12471ab9637d63fd6f
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2023-03-22 12:18:24 -0300

    upds

--- engine/commands/server.php
+++ engine/commands/server.php
@@ -9,14 +9,16 @@ class Server extends Cli
 {
   public function init()
   {
-    $this->addCommand('start', function () {
+    $this->addCommand('start', function ($input) {
 
-      Utils::printLn("Starting server at localhost in port 8000.");
+      $port = empty($input['port']) ? '8000' : $input['port'];
+
+      Utils::printLn("Starting server at localhost in port {$port}.");
       Utils::printLn();
       Utils::printLn("IMPORTANT: This server is intended for DEVELOPMENT PURPOSE ONLY. For production, we encourage you to use some solution like NGINX or APACHE web server.");
       Utils::printLn();
 
-      exec('php -S 0.0.0.0:8000 -t '.ROOT_PATH.'/public');
+      exec("php -S 0.0.0.0:{$port} -t ".ROOT_PATH.'/public');
     });
   }
 }
